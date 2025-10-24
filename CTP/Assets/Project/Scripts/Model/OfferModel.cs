6ebc16323f2f72b5c57c820ee7ccf7ffd64c61a6
Mikhail <>
2025-10-24 18:59:44 +0300

    - сделал человечью проверку на лимит покупок

--- CTP/Assets/Project/Scripts/Model/OfferModel.cs
+++ CTP/Assets/Project/Scripts/Model/OfferModel.cs
@@ -5,16 +5,24 @@ namespace RedPanda.Project.Scripts.Model
     public sealed class OfferModel
     {
         public OfferConfig Config { get; private set; }
-        public int BuyCount { get; private set; }
+
+        private int _buyCount;
 
         public OfferModel(OfferConfig config)
         {
             Config = config;
+            _buyCount = 0;
         }
 
         public void OnBuy()
         {
-            BuyCount++;
+            _buyCount++;
+        }
+
+        public bool IsBuyLimitReached()
+        { 
+            var result = _buyCount >= Config.BuyLimit;
+            return result;
         }
     }
 }
