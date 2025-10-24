1f8d8533163888ad0e07a3057a0ff4b914078436
Mikhail <>
2025-10-24 18:58:40 +0300

    - ренейм Game -> GameController (чтобы не писать Game.Game)

--- CTP/Assets/Project/Scripts/Game/GameController.cs
+++ CTP/Assets/Project/Scripts/Game/GameController.cs
@@ -1,3 +1,4 @@
+using RedPanda.Project.Scripts.EventBus.Async;
 using RedPanda.Project.Scripts.Model;
 using RedPanda.Project.Scripts.Services;
 using RedPanda.Project.Scripts.UI;
@@ -5,11 +6,12 @@ using UnityEngine;
 
 namespace RedPanda.Project.Scripts.Game
 {
-    public class Game : MonoBehaviour
+    public class GameController : MonoBehaviour
     {
         public static UserModel User { get; private set; }
         public static ShopModel Shop { get; private set; }
         public static IShopService ShopService { get; private set; }
+        public static IEventBusAsync EventBus { get; private set; }
 
         [SerializeField] private ShopView _shopView;
 
@@ -18,6 +20,7 @@ namespace RedPanda.Project.Scripts.Game
             User = new UserModel();
             Shop = new ShopModel();
             ShopService = new ShopService();
+            EventBus = new EventBusAsync();
         }
 
         private void Start()
