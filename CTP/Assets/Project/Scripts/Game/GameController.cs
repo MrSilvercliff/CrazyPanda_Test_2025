4c57fa13449aec057b1556e098e2ccfff491fd22
Mikhail <>
2025-10-25 16:50:28 +0300

    - подгрузка окна магазина из аддрессаблов

--- CTP/Assets/Project/Scripts/Game/GameController.cs
+++ CTP/Assets/Project/Scripts/Game/GameController.cs
@@ -2,27 +2,33 @@ using RedPanda.Project.Scripts.Configs;
 using RedPanda.Project.Scripts.EventBus.Async;
 using RedPanda.Project.Scripts.Model;
 using RedPanda.Project.Scripts.Services;
+using RedPanda.Project.Scripts.Services.UnityAddressables;
 using RedPanda.Project.Scripts.UI;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace RedPanda.Project.Scripts.Game
 {
     public class GameController : MonoBehaviour
     {
-        [SerializeField] private ShopView _shopView;
-
         private IUserModel _userModel;
+        private IAddressablesConfig _addressablesConfig;
+        private IAddressablesService _addressablesService;
 
         private void Awake()
         {
         }
 
-        private void Start()
+        private async void Start()
         {
             _userModel = GameCore.Instance.User;
+            _addressablesConfig = GameCore.Instance.AddressablesConfig;
+            _addressablesService = GameCore.Instance.AddressablesService;
 
-            _shopView.Init();
-            _shopView.gameObject.SetActive(true);
+            var shopView = await _addressablesService.LoadShopViewAsync(_addressablesConfig.ShopView);
+            var instance = Instantiate(shopView);
+            instance.Init();
+            instance.gameObject.SetActive(true);
         }
 
         private void Update()
