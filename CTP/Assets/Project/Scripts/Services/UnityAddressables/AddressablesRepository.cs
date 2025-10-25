1dd8454b22fb9f0fb27765c6a2a3028434796e71
Mikhail <>
2025-10-25 16:48:57 +0300

    - сервисы аддрессаблов

--- CTP/Assets/Project/Scripts/Services/UnityAddressables/AddressablesRepository.cs
+++ CTP/Assets/Project/Scripts/Services/UnityAddressables/AddressablesRepository.cs
@@ -1,12 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace RedPanda.Project.Scripts.Services.UnityAddressables
 {
     public interface IAddressablesRepository
-    { 
+    {
+        IReadOnlyCollection<AsyncOperationHandle> GetAll();
+
+        bool TryGet<T>(string guid, out T result);
+        void Add(string guid, AsyncOperationHandle asyncOperationHandle);
+        void Remove(string guid);
     }
 
     public class AddressablesRepository : IAddressablesRepository
     {
+        private Dictionary<string, AsyncOperationHandle> _operationHandleByGUID;
+
+        public AddressablesRepository()
+        {
+            _operationHandleByGUID = new();
+        }
+
+        public IReadOnlyCollection<AsyncOperationHandle> GetAll()
+        {
+            var result = _operationHandleByGUID.Values;
+            return result;
+        }
+
+        public bool TryGet<T>(string guid, out T result)
+        {
+            result = default(T);
+            var tryResult = _operationHandleByGUID.TryGetValue(guid, out var asyncOperationHandle);
+
+            if (!tryResult)
+                return false;
+
+            return true;
+        }
+
+        public void Add(string guid, AsyncOperationHandle asyncOperationHandle)
+        {
+            _operationHandleByGUID[guid] = asyncOperationHandle;
+        }
+
+        public void Remove(string guid)
+        {
+            _operationHandleByGUID.Remove(guid);
+        }
     }
 }
--- CTP/Assets/Project/Scripts/Services/UnityAddressables/AddressablesService.cs
+++ CTP/Assets/Project/Scripts/Services/UnityAddressables/AddressablesService.cs
@@ -1,9 +1,15 @@
+using RedPanda.Project.Scripts.UI;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace RedPanda.Project.Scripts.Services.UnityAddressables
 {
     public interface IAddressablesService
     { 
+        Task<ShopView> LoadShopViewAsync(AssetReferenceGameObject assetReference);
+        Task<GameObject> LoadGameObjectAsync(AssetReferenceGameObject assetReference);
+        Task<Sprite> LoadSpriteAsync(AssetReferenceSprite assetReference);
     }
 
     public class AddressablesService : IAddressablesService
@@ -15,6 +21,37 @@ namespace RedPanda.Project.Scripts.Services.UnityAddressables
         { 
             _repository = new AddressablesRepository();
             _loadService = new AddressablesLoadService(_repository);
+        }
+
+        public async Task<ShopView> LoadShopViewAsync(AssetReferenceGameObject assetReference)
+        {
+            var shopViewGameObject = await LoadAsync<GameObject>(assetReference);
+            var result = shopViewGameObject.GetComponent<ShopView>();
+            return result;
+        }
+
+        public async Task<GameObject> LoadGameObjectAsync(AssetReferenceGameObject assetReference)
+        {
+            var result = await LoadAsync<GameObject>(assetReference);
+            return result;
+        }
+
+        public async Task<Sprite> LoadSpriteAsync(AssetReferenceSprite assetReference)
+        {
+            var result = await LoadAsync<Sprite>(assetReference);
+            return result;
+        }
+
+        private async Task<T> LoadAsync<T>(AssetReference assetReference)
+        {
+            var guid = assetReference.AssetGUID;
+            var tryResult = _repository.TryGet<T>(guid, out var result);
+
+            if (tryResult)
+                return result;
+
+            result = await _loadService.LoadAsync<T>(assetReference);
+            return result;
         }
     }
 }
