f441dc30b26757f343c241af84e15576ce188714
Mikhail <>
2025-10-26 14:38:11 +0300

    - забыл интерфейс

--- CTP/Assets/Project/Scripts/UI/Widgets/OfferWidgetHybridScrollDrag.cs
+++ CTP/Assets/Project/Scripts/UI/Widgets/OfferWidgetHybridScrollDrag.cs
@@ -1,23 +1,36 @@
 using RedPanda.Project.Scripts.Model;
+using RedPanda.Project.Scripts.ObjectPool;
 using RedPanda.Project.Scripts.UI.UtilWigets;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace RedPanda.Project.Scripts.UI.Widgets
 {
-    public class OfferWidgetHybridScrollDrag : MonoBehaviour
+    public class OfferWidgetHybridScrollDrag : MonoBehaviour, IPoolable
     {
         [SerializeField] private OfferWidget _offerWidget;
         [SerializeField] private HybridScrollRectDragPointerHandleWidget _hybridScrollRect;
 
         public void Setup(OfferModel offerModel)
-        { 
+        {
             _offerWidget.Setup(offerModel);
         }
 
         public void Setup(ScrollRect verticalScrollRect, ScrollRect horizontalScrollRect)
-        { 
+        {
             _hybridScrollRect.Setup(verticalScrollRect, horizontalScrollRect);
+        }
+
+        public void OnCreate()
+        {
+        }
+
+        public void OnSpawn()
+        {
+        }
+
+        public void OnDespawn()
+        {
         }
     }
 }
